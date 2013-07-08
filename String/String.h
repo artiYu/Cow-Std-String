2241c8ce9ace5dc61e41918b27b1ea204a5dc364
Yuliya <lapl4rt@gmail.com>
2013-07-08 17:58:11 +0400

    Transfer some functions in String/String.h as virtual functions

--- String/String.h
+++ String/String.h
@@ -5,6 +5,11 @@ public:
   virtual ~String();
   static String* create(int type, const char *sz = "");
 
+  virtual const String& operator+=(const char *sz) = 0;
+  //friend String operator+(const char *, const String &ns);
+
+  virtual char& operator[](int index) = 0;
+  virtual const char operator[](int index) const = 0;
   virtual operator const char*() const = 0;
 
   virtual bool operator==(String &str_sz) const;
